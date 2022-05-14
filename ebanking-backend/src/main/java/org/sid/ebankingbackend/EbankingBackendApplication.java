38809267ddb150a74e6d74cdbd5c58027a914cf5
amina <dahmouniamina.contact@gmail.com>
2022-05-14 08:28:07 +0100

    first commit

---
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/EbankingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.sid.ebankingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankingBackendApplication.class, args);
+    }
+
+}
